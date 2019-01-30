f3d50b52ce4244e5a618999b46b10f6b6af594f8
iamangeldin <iamangeldin@bk.ru>
2019-01-30 21:21:48 +0700

    Task2

---
+++ Week 1/Task2/Task2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public int year;
+
+        public Student()
+        {
+            name = "noName";
+            id = "0";
+            year = 0;
+        }
+        public Student(string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+            this.year = 1;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+        public string getId()
+        {
+            return this.id;
+        }
+        public void incYear()
+        {
+            this.year++;
+        }
+        public override string ToString()
+        {
+            return name + " " + id + " " + year;
+        }
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            Student s2 = new Student("Islam", "18BD110565");
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+            Console.WriteLine(s1.getName());
+            Console.WriteLine(s2.getName());
+
+            Console.WriteLine(s1.getId());
+            Console.WriteLine(s2.getId());
+
+            s1.incYear();
+            s2.incYear();
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+
+            Console.ReadKey();
+        }
+    }
+    }
+}
