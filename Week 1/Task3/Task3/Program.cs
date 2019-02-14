5a6253d60c6dc08628451e40682415fb4251156c
iamangeldin <iamangeldin@bk.ru>
2019-02-15 02:49:04 +0700

    First

---
+++ Week 1/Task3/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine(); // Reading string 
+            int a = int.Parse(s); // We converted to int
+            string d = Console.ReadLine(); // Creating sequence of numbers
+            string[] num = d.Split(); // Creating array and spliting numbers by space
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write(num[i] + " " + num[i] + " "); // Just duplicate that numbers
+            }
+        }
+    }
+}
---
+++ Week 1/Task4/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine()); /// giving a range
+            string[,] arr = new string[a, a]; // creating 2d array
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < i + 1; j++) // j-th going by i-th
+                {
+                    arr[i, j] = "[*]"; // star
+
+                    Console.Write(arr[i, j]); // line
+                }
+                Console.WriteLine(); // ending an line
+            }Console.ReadKey();
+        } 
+    }
+}
