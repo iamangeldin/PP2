9a23c6abc4a86e15d3b030eaf089b19fed93728a
iamangeldin <iamangeldin@bk.ru>
2019-02-15 18:14:42 +0700

    12345

---
+++ Week 3/Far1/Far1/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace Far1
+{
+    class Far1
+    {
+        private int cursor;
+        public int Cursor
+        {
+            get
+            {
+                return cursor;
+            }
+            set
+            {
+                if (value >= Vector.Count)
+                {
+                    cursor = 0;
+                }
+                else if (value < 0)
+                {
+                    cursor = Vector.Count - 1;
+                }
+                else
+                {
+                    cursor = value;
+                }
+            }
+        }
+
+
+        public List<FileSystemInfo> Vector
+        {
+            get;
+            set;
+        }
+        public void Draw()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Black;
+            for (int i = 0; i < Vector.Count; ++i)
+            {
+                if (i == Cursor)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine(Vector[i].Name);
+            }
+        }
+        public void Delete()
+        {
+            FileSystemInfo fileSystemInfo = Vector[cursor];
+            if (fileSystemInfo.GetType() == typeof(DirectoryInfo))
+            {
+                Directory.Delete(fileSystemInfo.FullName, true);
+            }
+            else
+            {
+                File.Delete(fileSystemInfo.FullName);
+            }
+            Vector.RemoveAt(cursor);
+            cursor--;
+        }
+    }
+    enum Type
+    {
+        File,
+        Directory
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type typ = Type.Directory;
+            string path = @"C:\Users\Админ\Desktop\PP2";
+            DirectoryInfo dir = new DirectoryInfo(path);
+            Stack<Far1> par = new Stack<Far1>();
+            par.Push(
+                new Far1
+                {
+                    Vector = dir.GetFileSystemInfos().ToList(),
+                    Cursor = 0
+                });
+            while (true)
+            {
+                par.Peek().Draw();
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.F2:
+                        int y = par.Peek().Cursor;
+                        FileSystemInfo fos = par.Peek().Vector[y];
+                        string chan = Console.ReadLine();
+                        DirectoryInfo df = new DirectoryInfo(fos.FullName);
+                        File.Move(fos.FullName, df.Parent.FullName + "/" + chan);
+                        break;
+                    case ConsoleKey.Delete:
+                        par.Peek().Delete();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        par.Peek().Cursor--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        par.Peek().Cursor++;
+                        break;
+                    case ConsoleKey.Backspace:
+                        if (typ == Type.Directory)
+                        {
+                            par.Pop();
+                        }
+                        else
+                        {
+                            typ = Type.Directory;
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                        }
+                        break;
+                    case ConsoleKey.Enter:
+                        int x = par.Peek().Cursor;
+                        FileSystemInfo fileSystemInfo = par.Peek().Vector[x];
+                        if (fileSystemInfo.GetType() == typeof(DirectoryInfo))
+                        {
+                            DirectoryInfo directoryInfo = fileSystemInfo as DirectoryInfo;
+                            par.Push(
+                                new Far1
+                                {
+                                    Vector = directoryInfo.GetFileSystemInfos().ToList(),
+                                    Cursor = 0
+                                });
+                        }
+                        else
+                        {
+                            Process.Start(fileSystemInfo.FullName);
+                        }
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Week 3/Far2/Far2/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FarManager
+{
+    class FarManager
+    {
+        public int cursor;
+        public int size;
+        public string path;
+        DirectoryInfo DOP = null; //создаем пустую папку
+        FileSystemInfo CUP = null;//создаем пустой файл
+        public FarManager()// тут исползуем конструкторы
+        {
+            cursor = 0;
+        }
+
+        public FarManager(string path)
+        {
+            this.path = path;
+            cursor = 0;
+            DOP = new DirectoryInfo(path);
+            size = DOP.GetFileSystemInfos().Length;
+        }
+        public void UP()
+        {
+            cursor--;
+            if (cursor < 0)
+            {
+                cursor = size - 1;
+            }
+        }
+        public void DOWN()
+        {
+            cursor++;
+            if (cursor >= size)
+            {
+                cursor = 0;
+            }
+        }
+        public void COLOR(FileSystemInfo fs, int index)
+        {
+            if (cursor == index)
+            {
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                CUP = fs;
+
+            }
+            else if (fs.GetType() == typeof(FileInfo))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+        }
+        public void ENTER()
+        {
+            DOP = new DirectoryInfo(path);
+            FileSystemInfo[] files = DOP.GetFileSystemInfos();
+            size = files.Length;
+            int k = 0;
+            foreach (FileSystemInfo fs in files)
+            {
+                COLOR(fs, k);
+                Console.Write(k + 1 + ". ");
+                Console.WriteLine(fs.Name);
+                k++;
+            }
+        }
+        public void FAR()
+        {
+            ConsoleKeyInfo consoleKey = Console.ReadKey();
+            while (true)
+            {
+                Console.Clear();
+                ENTER();
+                consoleKey = Console.ReadKey();
+                if (consoleKey.Key == ConsoleKey.UpArrow)
+                {
+                    UP();
+                }
+                if (consoleKey.Key == ConsoleKey.DownArrow)
+                {
+                    DOWN();
+                }
+                if (consoleKey.Key == ConsoleKey.Enter)
+                {
+                    if (CUP.GetType() == typeof(DirectoryInfo))
+                    {
+                        cursor = 0;
+                        path = CUP.FullName;
+                    }
+                    else
+                    {
+
+                    }
+                }
+                if (consoleKey.Key == ConsoleKey.Delete)
+                {
+
+                }
+                if (consoleKey.Key == ConsoleKey.Escape)
+                {
+                    cursor = 0;
+                    path = DOP.Parent.FullName;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = (@"C:\Users\Админ\Desktop");
+            FarManager farManager = new FarManager(path);
+            farManager.FAR();
+        }
+    }
+}
